2a2c079cec9dd72cc05e509069f42be7f5e1de06
Ricardas007 <rychmaister@gmail.com>
2024-05-06 18:07:14 +0300

    Test documentation updated and intelij enviroment created

---
+++ test-project-managment-tool/src/test/java/lt/codeCrusaders/testProjectManagmentTool/BaseTest.java
@@ -0,0 +1,25 @@
+package lt.codeCrusaders.testProjectManagmentTool;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost:5173/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @AfterEach
+    void closeDown() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.quit();
+    }
+}
